81dc12ba7a5b966083ef64439f63186f0d46c357
Nicolas Ibanheiz <Nicolas Ibanheiz@Ibanheiz>
2013-10-20 18:26:04 -0200

    - Indentando código.

--- src/main/java/com/irssolucoes/domain/repository/ClienteRepository.java
+++ src/main/java/com/irssolucoes/domain/repository/ClienteRepository.java
@@ -2,11 +2,11 @@ package com.irssolucoes.domain.repository;
 
 import com.irssolucoes.domain.entity.Cliente;
 import java.util.List;
-
 /**
  *
  * @author Nicolas Ibanheiz
  */
-public interface ClienteRepository extends Repository<Cliente>{
+public interface ClienteRepository extends Repository<Cliente> {
+
     List<Cliente> listAll();
 }
